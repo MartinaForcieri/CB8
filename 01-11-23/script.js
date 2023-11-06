aa2e2940a38a9b13dce27a724c1ea3a9eb0808a2
MartinaForcieri <148761715+MartinaForcieri@users.noreply.github.com>
2023-11-06 09:52:15 +0100

    Add files via upload

---
+++ 01-11-23/script.js
@@ -0,0 +1,14 @@
+const animali = [
+  "Lepre",
+  "Corvo",
+  "Serpente",
+  "Tigre",
+  "Orso",
+  "Canguro",
+  "Elefante",
+  "Giraffa",
+];
+
+for (let i = 0; i < animali.length; i++) {
+  console.log(`${i + 1}: ${animali[i]}`);
+}
