3b96c7898c28f24839e866a62ad3ff0e28613e45
Martina Forcieri <148761715+MartinaForcieri@users.noreply.github.com>
2024-01-18 11:59:49 +0100

    Add files via upload

---
+++ 17-01-24/react-router/src/routes/contacts.jsx
@@ -0,0 +1,13 @@
+// Contacts.jsx
+import React from "react";
+
+const Contacts = () => {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>This is the Contacts page content.</p>
+    </div>
+  );
+};
+
+export default Contacts;
