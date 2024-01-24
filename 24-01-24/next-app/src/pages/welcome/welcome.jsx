9f8d14bd41d24e53336ee6b02de8a0f62d8e5d9e
Martina Forcieri <148761715+MartinaForcieri@users.noreply.github.com>
2024-01-24 10:13:12 +0100

    Add files via upload

---
+++ 24-01-24/next-app/src/pages/welcome/welcome.jsx
@@ -0,0 +1,19 @@
+import styles from "./index.module.scss";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome Player</h1>
+      <p>
+        Welcome PLAYER, this tool is the Pokedex will allow you to tour the
+        world and discover the fantastic creatures that are the Pokemon. ENJOY!
+      </p>
+      <Image
+        src="https://miro.medium.com/v2/resize:fit:600/format:webp/0*NsHVyfpJ2k7ixTAd"
+        alt="pokedex"
+        width="1000"
+        height="600"
+      />
+    </div>
+  );
+};
